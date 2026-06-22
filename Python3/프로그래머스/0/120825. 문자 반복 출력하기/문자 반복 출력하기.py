58fd8671ea9b2d0dc77ef550e4f74f0d18fe65e5
Jindevelopment <jinhyeok1001@naver.com>
2026-06-22 15:50:10 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.01 ms, Memory: 11.6 MB -BaekjoonHub

--- Python3/프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
+++ Python3/프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
@@ -1,6 +1,8 @@
 def solution(my_string, n):
     answer = ''
+    
     for i in range(len(my_string)):
-        for k in range(n):
-            answer+=my_string[i]
+        for j in range(n):
+            answer += my_string[i]
+    
     return answer
